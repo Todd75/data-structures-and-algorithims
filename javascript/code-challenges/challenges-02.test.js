05ab0985c0fcda45375a13d87c4279e4964a4f2d
Todd Wolden <tcwolden@gmail.com>
2022-11-08 19:58:44 -0700

    Challenge 3 passed... was pretty intuitive after question2

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -34,7 +34,8 @@ Write a function named addQuestion that, given an array of strings, uses map to 
 ------------------------------------------------------------------------------------------------ */
 
 const addQuestion = (arr) => {
-  // Solution code here...
+  let newArray = arr.map(string => string + '?');
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
